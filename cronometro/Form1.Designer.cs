a3c84448508e27688716df0ff96bfbb2675526ed
AngelLira11 <prodigylira777@gmail.com>
2025-02-09 13:27:17 -0600

    Agregar archivos de proyecto.

---
+++ cronometro/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace cronometro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnPausa = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblXronos = new System.Windows.Forms.Label();
+            this.tmrCrono = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.Location = new System.Drawing.Point(218, 189);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(121, 52);
+            this.btnIniciar.TabIndex = 0;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnPausa
+            // 
+            this.btnPausa.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPausa.Location = new System.Drawing.Point(345, 189);
+            this.btnPausa.Name = "btnPausa";
+            this.btnPausa.Size = new System.Drawing.Size(119, 52);
+            this.btnPausa.TabIndex = 1;
+            this.btnPausa.Text = "Pausa";
+            this.btnPausa.UseVisualStyleBackColor = true;
+            this.btnPausa.Click += new System.EventHandler(this.btnPausa_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(470, 189);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(119, 52);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblXronos
+            // 
+            this.lblXronos.AutoSize = true;
+            this.lblXronos.Font = new System.Drawing.Font("Wide Latin", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblXronos.Location = new System.Drawing.Point(47, 44);
+            this.lblXronos.Name = "lblXronos";
+            this.lblXronos.Size = new System.Drawing.Size(714, 98);
+            this.lblXronos.TabIndex = 3;
+            this.lblXronos.Text = "00:00:00";
+            // 
+            // tmrCrono
+            // 
+            this.tmrCrono.Enabled = true;
+            this.tmrCrono.Interval = 1000;
+            this.tmrCrono.Tick += new System.EventHandler(this.tmrCrono_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(920, 307);
+            this.Controls.Add(this.lblXronos);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnPausa);
+            this.Controls.Add(this.btnIniciar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Cronometro";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnPausa;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblXronos;
+        private System.Windows.Forms.Timer tmrCrono;
+    }
+}
+
---
+++ cronometro/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cronometro
+{
+    public partial class Form1 : Form
+    {
+        int hh, mm, ss;
+        string m, s, mostrar;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblXronos.Text = "00:00:00";
+        }
+
+        public void reiniciar()
+        {
+            hh =0; mm = 0; ss = 0;
+            lblXronos.Text = "00:00:00";
+            tmrCrono.Stop();
+           
+        }
+
+        private void btnPausa_Click(object sender, EventArgs e)
+        {
+            tmrCrono.Stop();
+        }
+
+        private void tmrCrono_Tick(object sender, EventArgs e)
+        {
+            ss += 1;
+            s = Convert.ToString( ss);
+            m = Convert.ToString(mm);
+
+
+            if (ss <10)
+            {
+                s = Convert.ToString("0" + ss);
+            }
+            if (mm < 10)
+            {
+                m = Convert.ToString("0" + mm);
+            }
+
+            lblXronos.Text = Convert.ToString(hh + " : " + m + " : " + s);
+
+            if (ss >= 59)
+            {
+                mm += 1;
+                ss = 0;
+
+                if (mm >= 59)
+                {
+                    hh += 1;
+                    mm = 0;
+                }
+            }
+
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            tmrCrono.Start();
+
+        }
+
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            
+            reiniciar();
+            
+        }
+    }
+}
